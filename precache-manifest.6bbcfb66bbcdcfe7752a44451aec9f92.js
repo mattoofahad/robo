ac54c6cd52854903187aa452ecef9f98f197b4a1
mattoofahad <mattoofahad@gmail.com>
2020-09-06 04:32:18 +0500

    Updates

---
+++ precache-manifest.6bbcfb66bbcdcfe7752a44451aec9f92.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e31ed393ea390159de54615df956902",
+    "url": "/robo/index.html"
+  },
+  {
+    "revision": "5b48dd57267ff866d768",
+    "url": "/robo/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "ab5a302f00256c90486a",
+    "url": "/robo/static/css/main.37d749e9.chunk.css"
+  },
+  {
+    "revision": "5b48dd57267ff866d768",
+    "url": "/robo/static/js/2.c2fcf533.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robo/static/js/2.c2fcf533.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab5a302f00256c90486a",
+    "url": "/robo/static/js/main.ad385817.chunk.js"
+  },
+  {
+    "revision": "59ac14711f3d62421fd7",
+    "url": "/robo/static/js/runtime-main.2b9bdbda.js"
+  },
+  {
+    "revision": "76c1ccc6b25b11b639a0d3789020b39d",
+    "url": "/robo/static/media/Hawkeyes Free.76c1ccc6.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-app/precache-manifest.d89c42841a5ba9d251ac4ce91da8bed5.js"
+  "/robo/precache-manifest.6bbcfb66bbcdcfe7752a44451aec9f92.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/robo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
