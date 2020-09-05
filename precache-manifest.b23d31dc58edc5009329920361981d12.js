28ee71b3b0b8e23c8be5efd3a0de59af65bdcb41
mattoofahad <mattoofahad@gmail.com>
2020-09-06 04:18:25 +0500

    Updates

---
+++ precache-manifest.b23d31dc58edc5009329920361981d12.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee15de61b8561bbf9d8d05acfae738d0",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "5b48dd57267ff866d768",
+    "url": "/my-app/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "8f01a0569f08ff800343",
+    "url": "/my-app/static/css/main.5e5ef9fc.chunk.css"
+  },
+  {
+    "revision": "5b48dd57267ff866d768",
+    "url": "/my-app/static/js/2.c2fcf533.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.c2fcf533.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f01a0569f08ff800343",
+    "url": "/my-app/static/js/main.d3c77627.chunk.js"
+  },
+  {
+    "revision": "55f3522b18ce8b03bed6",
+    "url": "/my-app/static/js/runtime-main.76eb41e5.js"
+  },
+  {
+    "revision": "76c1ccc6b25b11b639a0d3789020b39d",
+    "url": "/my-app/static/media/Hawkeyes Free.76c1ccc6.woff"
+  }
+]);
