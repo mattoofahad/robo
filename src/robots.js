5f1b66038a07773ec50f57117b89c834edb8c46c
mattoofahad <mattoofahad@gmail.com>
2020-09-06 03:16:25 +0500

    Adding files

---
+++ src/robots.js
@@ -0,0 +1,58 @@
+export const robots =[
+{
+	id: 12,
+	name: 'M-Robo mike V1',
+	username: 'V1',
+	email: 'V1-Robo_Mail@MDweb.com' 
+},
+{
+	id: 23,
+	name: 'M-Robo ash V1',
+	username: 'V2',
+	email: 'V2-Robo_Mail@MDweb.com' 
+},
+{
+	id: 34,
+	name: 'M-Robo tom V1',
+	username: 'V3',
+	email: 'V3-Robo_Mail@MDweb.com' 
+},
+{
+	id: 45,
+	name: 'M-Robo tom V2',
+	username: 'V4',
+	email: 'V4-Robo_Mail@MDweb.com' 
+},
+{
+	id: 56,
+	name: 'M-Robo elvis V5',
+	username: 'V5',
+	email: 'V5-Robo_Mail@MDweb.com' 
+},
+{
+	id: 67,
+	name: 'M-Robo naika V6',
+	username: 'V6',
+	email: 'V6-Robo_Mail@MDweb.com' 
+},
+{
+	id: 78,
+	name: 'M-Robo defko V7',
+	username: 'V7',
+	email: 'V7-Robo_Mail@MDweb.com' 
+},
+
+{
+	id: 89,
+	name: 'M-Robo simpo V8',
+	username: 'V8',
+	email: 'V8-Robo_Mail@MDweb.com' 
+},
+{
+	id: 90,
+	name: 'M-Robo jimjim V9',
+	username: 'V9',
+	email: 'V9-Robo_Mail@MDweb.com' 
+},
+
+]
